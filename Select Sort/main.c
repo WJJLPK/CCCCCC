788a66db25b8e3064e58cb99ffb758288521cc66
WJJWYF <45487976+WJJWYF@users.noreply.github.com>
2018-12-07 20:57:10 +0800

    选择排序

---
+++ Select Sort/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,temp;
+    int a[10];
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
