bff447578c422576b7c4b69bef7602a93e4e9e33
WJJWYF <45487976+WJJWYF@users.noreply.github.com>
2018-12-07 20:26:24 +0800

    冒泡

---
+++ Bubble Sort/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[10];
+    int i,j;
+    int temp;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+        printf("%d",a[i]);
+}
