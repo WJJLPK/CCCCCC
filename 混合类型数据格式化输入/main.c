e271f895b828253bcee9c788d86852ce52d80d40
WJJWYF <45487976+WJJWYF@users.noreply.github.com>
2018-12-21 20:31:27 +0800

    PTA

---
+++ 混合类型数据格式化输入/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+  float a;
+  int b;
+  char c;
+  float d;
+  scanf("%f %d %c %f",&a,&b,&c,&d);
+  printf("%c %d %.2f %.2f",c,b,a,d);
+
+}
