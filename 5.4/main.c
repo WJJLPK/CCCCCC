802066cdc45a085f3f674bd73aff0c7e0b50ca19
WJJWYF <45487976+WJJWYF@users.noreply.github.com>
2019-03-26 22:14:37 +0800

    出了点问题。。。

--- 5.4/main.c
+++ 5.4/main.c
@@ -8,23 +8,23 @@ int main()
    double n;
    printf("Please enter m and year:");
    scanf("%ld %d",&m,&year);
-   if(year=1)
+   if(year==1)
    {
        printf("%lf",n=m+m*0.0225);
    }
-   else if(year=2)
+   else if(year==2)
    {
        printf("%lf",n=m+m*0.0243*2);
    }
-   else if(year=3)
+   else if(year==3)
    {
        printf("%lf",n=m+m*0.0270*3);
    }
-    else if(year=5)
+    else if(year==5)
    {
        printf("%lf",n=m+m*0.0288*5);
    }
-    else if(year=8)
+    else if(year==8)
    {
        printf("%lf",n=m+m*0.0300*8);
    }
